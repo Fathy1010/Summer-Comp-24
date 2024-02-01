ea3e3dad3969a9770c0eef146e2eab69b51ff884
Jacob Hanna <21013107@students.latrobe.edu.au>
2024-02-01 22:29:11 +1100

    St Mark Yippie

--- Js/churchList.js
+++ Js/churchList.js
@@ -1,6 +1,7 @@
 const churchList = [
     ['St George Coptic Orthodox Church', '-37.7352489', '144.8061933', "77-79 Henry St, St Albans VIC 3021", '(03) 9364 5022', 'https://stgeorgecopts.org.au', 'admin@stgeorgecopts.org.au'],
     ['St Mary Coptic Orthodox Church', '-37.789028', '144.922813', "1 Epsom Rd, Kensington VIC 3031", '(03) 9376 6651', 'https://www.stmc.org.au'],
+    ['St Mark Coptic Orthodox Church', '-37.749540', '144.988420', "40-42 Gilbert Rd, Preston VIC 3072", '1800 954 424', 'https://www.stmarkscocmelb.com', 'contact@stmarkscocmelb.com'],
   ];
   
   module.exports = churchList;
