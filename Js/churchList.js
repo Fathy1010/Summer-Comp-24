ca73f5f51d18a8417f7cabc5305608769cbef9a8
Jacob Hanna <21013107@students.latrobe.edu.au>
2024-02-02 19:32:11 +1100

    list update

--- Js/churchList.js
+++ Js/churchList.js
@@ -2,7 +2,7 @@ const churchList = [
     ['St George Coptic Orthodox Church', '-37.735248', '144.806193', "77-79 Henry St, St Albans VIC 3021", '(03) 9364 5022', 'https://stgeorgecopts.org.au'],
     ['St Mary Coptic Orthodox Church', '-37.789028', '144.922813', "1 Epsom Rd, Kensington VIC 3031", '(03) 9376 6651', 'https://www.stmc.org.au'],
     ['St Mark Coptic Orthodox Church', '-37.749540', '144.988420', "40-42 Gilbert Rd, Preston VIC 3072", '1800 954 424', 'https://www.stmarkscocmelb.com'],
-    ['St Verena & St Bishoy Coptic Church', '-37.810710', '144.960810', "Level 1/285 La Trobe St, Melbourne VIC 3000", '(03) 8872 8400', 'https://stverenaschurch.org/stverenastbishoyeporo.html'],
+    ['St Verena & St Bishoy Coptic Orthodox Church', '-37.810710', '144.960810', "Level 1/285 La Trobe St, Melbourne VIC 3000", '(03) 8872 8400', 'https://stverenaschurch.org/stverenastbishoyeporo.html'],
     ['St John & St Verena Coptic Orthodox Church', '-37.852690', '145.011580', "583 Orrong Rd, Armadale VIC 3143", '0414 650 060', 'https://stjohnthebelovedandstverenachurch.org/index.html'],
     ['St Macarius Coptic Orthodox Church', '-37.633899', '145.133239', "623/615-623 Yan Yean Rd, Yarrambat VIC 3091", '0411 780 444', 'https://www.hisvine.com/church/58/St-Macarius-Church'],
     ['St Athanasius Coptic Orthodox Church', '-37.796850', '145.197356', "100 Park Rd, Donvale VIC 3111", '(03) 8872 8400', 'https://www.stathanasius.org.au'],
@@ -10,7 +10,7 @@ const churchList = [
     ['St Bishoy and St Shenouda Coptic Orthodox Church', '-37.775792', '145.094765', "1-9 Prospect Rd, Bulleen VIC 3105", '1800 595 366', 'https://www.hisvine.com/church/27/St-Bishoy-St-Shenouda-Church'],
     ['St Mina and St Marina Coptic Orthodox Church', '-38.004752', '145.285377', "41-59 Saffron Dr, Hallam VIC 3805", '(03) 9796 5257', 'http://www.hallamcopts.org.au'],
     ['St Anthony Coptic Orthodox Monastery', '-36.927455', '144.792127', "Redburns Ln, Heathcote VIC 3523", '(03) 5433 3026', 'https://www.hisvine.com/church/55/St-Anthony-Monastery'],
-    ['Archangel Michael Monastery', '-37.322941', '144.558842', "Campaspe House, 29 Goldies Ln, Woodend VIC 3442", '(03) 5417 6353', 'https://www.hisvine.com/church/121/Archangel-Michael-Monastery'],
+    ['Archangel Michael Coptic Orthodox Monastery', '-37.322941', '144.558842', "Campaspe House, 29 Goldies Ln, Woodend VIC 3442", '(03) 5417 6353', 'https://www.hisvine.com/church/121/Archangel-Michael-Monastery'],
     
   ];
   
