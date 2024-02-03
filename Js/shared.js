9656282d6a0663bd59344a8318f455819dc77146
fabd0009 <fabd0009@student.monash.edu>
2024-02-03 21:43:08 +1100

    reformatted the files to have the most updated button options along with the card formation and uploaded the movie and the pdf file

--- Js/shared.js
+++ Js/shared.js
@@ -5,141 +5,8 @@ const BOOKINGBOOK_KEY = "bookingBookStorage";
 
 // Classes
 
-class Booking {
-    constructor() {
-        this._fullName = "";
-        this._startAddress = "";
-        this._startAddressDesc = "";
-        this._stops = [];
-        this._stopsDesc = [];
-        this._estimatedFare = 0;
-        this._taxiType = "";
-        this._distance = 0;
-        this._isScheduled = false;
-        this._bookingDate = "";
-        this._bookingTime ="";
-        this._isCompleted = false;
-    }
-    // Accessors
-    get fullName() {
-        return this._fullName;
-    }
-    get startAddress() {
-        return this._startAddress;
-    }
-    get startAddressDesc() {
-        return this._startAddressDesc;
-    }
-    get stops() {
-        return this._stops;
-    }
-    get stopsDesc() {
-        return this._stopsDesc;
-    }
+class Travel {
 
-    get estimatedFare() {
-        return this._estimatedFare;
-    }
-
-    get taxiType() {
-        return this._taxiType;
-    }
-    get distance() {
-        return this._distance;
-    }
-
-    get isScheduled() {
-        return this._isScheduled;
-    }
-
-    get bookingDate() {
-        return this._bookingDate;
-    }
-
-    get bookingTime() {
-        return this._bookingTime;
-    }
-
-    get isCompleted() {
-        return this._isCompleted;
-    }
-
-    //Mutators
-    set fullName(newFullName) {
-        this._fullName = newFullName;
-    }
-    set startAddress(newAddress) {
-        this._startAddress = newAddress;
-    }
-    set startAddressDesc(newAddressDesc) {
-        this._startAddressDesc = newAddressDesc;
-    }
-    set stops(newStop) {
-        this._stops = newStop;
-    }
-    set stopsDesc(newStopDesc) {
-        this._stopsDesc = newStopDesc;
-    }
-    set taxiType(newTaxiType) {
-        this._taxiType = newTaxiType;
-    }
-    set distance(newDistance) {
-        this._distance = newDistance;
-    }
-    set bookingDate(newBookingDate) {
-        this._bookingDate = newBookingDate;
-    }
-
-    set bookingTime(newBookingTime) {
-        this._bookingTime = newBookingTime;
-    }
-    //Methods
-    toggleScheduledStatus() {
-        if (this._isScheduled == true) {
-            this._isScheduled = false;
-        }
-        else {
-            this._isScheduled == true;
-        }
-    }
-
-    toggleCompletedStatus() {
-        if (this._isCompleted == true) {
-            this._isCompleted = false;
-        }
-        else {
-            this._isCompleted == true;
-        }
-    }
-
-    calculateFare(taxiType, distance) {
-        let flagRate = 4.20;
-        let distanceRate = 1.622;
-        let commercialLevy = 1.10;
-        let fare = flagRate + (distance * distanceRate) + commercialLevy;
-        let nightLevy = 0.2
-
-        let timeArray = this._bookingTime.split(":");
-        if (parseInt(timeArray[0])> 17 || parseInt(timeArray[0])< 9) {
-            fare += fare * nightLevy;
-        }
-
-        if(taxiType == "Sedan")
-        {
-            fare = fare;
-        }
-        else if (taxiType == "SUV") {
-            fare += 3.50;
-        }
-        else if (taxiType == "Van") {
-            fare += 6;
-        }
-        else if (taxiType = "Minibus") {
-            fare += 10;
-        }
-
-        this._estimatedFare = fare.toFixed(2);
-    }
     calculateDistance(lat1, lng1, lat2, lng2) {
 
         const R = 6371; // Earth radius in km
@@ -163,20 +30,6 @@ class Booking {
 
     removeDestinationAddress(index) {
         this._stops.splice(index, 1)
-    }
-
-    fromData(dataObject) {
-        this._fullName = dataObject._fullName;
-        this._startAddress = dataObject._startAddress;
-        this._startAddressDesc = dataObject._startAddressDesc
-        this._stops = dataObject._stops;
-        this._stopsDesc = dataObject._stopsDesc;
-        this._estimatedFare = dataObject._estimatedFare;
-        this._taxiType = dataObject._taxiType;
-        this._isScheduled = dataObject._isScheduled;
-        this._bookingDate = dataObject._bookingDate;
-        this._bookingTime = dataObject._bookingTime;
-        this._isCompleted = dataObject._isCompleted;
     }
 
 }
@@ -214,15 +67,6 @@ class BookingBook {
     }
 }
 
-//Functions - Buttons
-function viewAllBookingsButton() {
-    window.location = "bookingsPage.html";
-}
-
-function homeButton() {
-    window.location = "homePage.html";
-}
-
 // Functions = Local storage
 function localStorageCheck(key) {
     if (localStorage.getItem(key) === undefined) {
@@ -238,26 +82,5 @@ function localStorageUpdate(key, data) {
     localStorage.setItem(key, jsonData);
 }
 
-// function localStorageRetrieve(key) {
-//     let data = localStorage.getItem(key);
-//     try {
-//         data = JSON.parse(data);
-//     }
-//     catch (e) {
-
-//     }
-//     finally {
-//         return data;
-//     }
-// }
-
-// Functions = Retrival
-
-// function getBooking(BOOKING_KEY) {
-//     let data = localStorageRetrieve(BOOKING_KEY);
-//     let retrievedBooking = new Booking();
-//     retrievedBooking.fromData(data);
-//     return retrievedBooking;
-// }
 
 
